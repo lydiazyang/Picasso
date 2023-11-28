7eb8377b2841931d3f908177cb5fe2912ad04c9e
JENNA BERNSTEIN <BERNSTEINJ25@mail.wlu.edu>
2023-11-27 21:22:19 -0500

    add initial method and class stubs for the ceil function and the abs testing

---
+++ src/picasso/parser/language/expressions/Ceil.java
@@ -0,0 +1,9 @@
+package picasso.parser.language.expressions;
+
+public class Ceil {
+
+	public Ceil() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/picasso/parser/tokens/functions/CeilToken.java
@@ -0,0 +1,14 @@
+package picasso.parser.tokens.functions;
+
+public class CeilToken {
+
+	public CeilToken() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
