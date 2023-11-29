12f1d3da15f80b38ee6e4675c212c23f1b4383e8
JENNA BERNSTEIN <BERNSTEINJ25@mail.wlu.edu>
2023-11-29 11:24:06 -0500

    add more abs value tests

--- src/tests/EvaluatorTests.java
+++ src/tests/EvaluatorTests.java
@@ -72,24 +72,65 @@ public class EvaluatorTests {
 		Abs myTree = new Abs(new X());
 		
 		//straightforward tests
+		assertEquals(new RGBColor(0, 0, 0), myTree.evaluate(0, -1));
+		assertEquals(new RGBColor(1, 1, 1), myTree.evaluate(1, -1));
+		assertEquals(new RGBColor(1, 1, 1), myTree.evaluate(-1, -1));
+		assertEquals(new RGBColor(0.1, 0.1, 0.1), myTree.evaluate(0.1, -1));
+		assertEquals(new RGBColor(0.1, 0.1, 0.1), myTree.evaluate(-0.1, -1));
+		assertEquals(new RGBColor(.5, .5, .5), myTree.evaluate(0.5, -1));
+		assertEquals(new RGBColor(.5, .5, .5), myTree.evaluate(-0.5, -1));
 		
 		//test all integers
+		for (int i = -1; i <= 1; i++) {
+			i = Math.abs(i);
+			assertEquals(new RGBColor(i, i, i), myTree.evaluate(i, -i));
+			assertEquals(new RGBColor(i, i, i), myTree.evaluate(i, i));
+		}
 		
 		//test doubles
 		double[] tests = {-.7, -0.00001, 1.3, -4.9999};
 		for (double testVal : tests) {
 			double absOfTestVal = Math.abs(testVal);
 			assertEquals(new RGBColor(absOfTestVal, absOfTestVal, absOfTestVal), myTree.evaluate(testVal, -1));
+			assertEquals(new RGBColor(absOfTestVal, absOfTestVal, absOfTestVal), myTree.evaluate(testVal, testVal));
+			assertEquals(new RGBColor(absOfTestVal, absOfTestVal, absOfTestVal), myTree.evaluate(testVal, 1));
 		}
+	}
+	
+	@Test
+	public void testWrapEvaluation() {
+		Wrap myTree = new Wrap(new X());
+		
+		// test "normal" cases
+		assertEquals(new RGBColor(0, 0, 0), myTree.evaluate(0, -1));
+		assertEquals(new RGBColor(1, 1, 1), myTree.evaluate(1, -1));
+		assertEquals(new RGBColor(-1, -1, -1), myTree.evaluate(-1, -1));
+		assertEquals(new RGBColor(-.5, -.5, -.5), myTree.evaluate(1.5, -1));
+		assertEquals(new RGBColor(.5, .5, .5), myTree.evaluate(-1.5, -1));
+		
+		// test all ints from -60 to 60
+		
+		// test doubles 
+		double[] tests = {-1.66, -.34, .7888, 5.7222};
+		for (double testVal : tests) {
+			double wrappedTestVal = testVal;
+			if (Math.abs(testVal) > 1) {
+				wrappedTestVal = testVal % 1;
+				// insert code to figure out how many times testVal needs to be "wrapped"
+				// -1.xx -> .xx
+				// 1.xx -> -.xx
+				// -2.xx -> -.xx
+				// 2.xx -> .xx
+			}
+			System.out.println(testVal + ": " + wrappedTestVal);
+			assertEquals(new RGBColor(wrappedTestVal, wrappedTestVal, wrappedTestVal), myTree.evaluate(testVal, -1));
+		}
+		
+
 	}
 
 	private void assertEquals(RGBColor rgbColor, RGBColor evaluate) {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	@Test
-	public void testAbsEvaluation() {
 		
 	}
 
