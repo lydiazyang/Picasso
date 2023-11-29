547c59bb56dfea34740267e09e2c1b9f0dc37ce7
Aiden Boeshans <boeshansa25@mail.wlu.edu>
2023-11-29 15:00:47 -0500

    Cleaned up comments and syntax for wrap function

--- src/picasso/parser/language/expressions/Wrap.java
+++ src/picasso/parser/language/expressions/Wrap.java
@@ -18,6 +18,8 @@ public class Wrap extends UnaryFunction {
 	}
 
 	/**
+	 * Evaluates the expression at the given x, y point by wrapping the value at that 
+	 * point around 0 (If the number exceeds 1 it wraps around 0, not -1 and vice versa)
 	 * 
 	 * @return the color from evaluating the wrapped value of the expression's parameter
 	 */
@@ -30,16 +32,25 @@ public class Wrap extends UnaryFunction {
 
 		return new RGBColor(red, green, blue);
 	}
-	
+	/**
+	 * Puts value through a while loop to subtract/add 1 until the value falls between
+	 * the correct maximum and minimum values
+	 * 
+	 * @param value the value to wrap
+	 * @return the wrapped value
+	 */
 	private double wrapValue(double value) {
+		// Set maximum and minimum integers
+		int max = 1;
+		int min = -1;		
 		// Wrapping helper function
 		double wrappedValue = Math.floor(value);
-		// Wrap negative values
-		while (wrappedValue < -1) {
+		// Wrap values lower than minimum
+		while (wrappedValue < min) {
 			wrappedValue += 1;
 		}
-		// Wrap positive values
-		while (wrappedValue > 1) {
+		// Wrap values higher than maximum
+		while (wrappedValue > max) {
 			wrappedValue -= 1;
 		}
 		return wrappedValue;
