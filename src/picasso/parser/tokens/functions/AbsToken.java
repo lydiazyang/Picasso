2603f44d7f5417abb8eb5d8acaa7d7034f7d8b4b
JENNA BERNSTEIN <BERNSTEINJ25@mail.wlu.edu>
2023-11-30 22:18:45 -0500

    make class public

--- src/picasso/parser/tokens/functions/AbsToken.java
+++ src/picasso/parser/tokens/functions/AbsToken.java
@@ -4,7 +4,7 @@ package picasso.parser.tokens.functions;
  * Represents the abs function token
  * 
  */ 
-class AbsToken extends FunctionToken{
+public class AbsToken extends FunctionToken{
 
 	public AbsToken() {
 		super("Abs Function Token");
