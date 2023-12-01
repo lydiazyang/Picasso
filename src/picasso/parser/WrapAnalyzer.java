fadbea46facafcf79c4cc58d4561d415846e81f8
Aiden Boeshans <boeshansa25@mail.wlu.edu>
2023-11-30 22:24:22 -0500

    FIxed comments from WrapAnalyzer

--- src/picasso/parser/WrapAnalyzer.java
+++ src/picasso/parser/WrapAnalyzer.java
@@ -14,9 +14,7 @@ public class WrapAnalyzer extends UnaryFunctionAnalyzer {
 
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		tokens.pop(); // Need to remove the floor token
-		// the parameter is the next token(s) on the stack.
-		// But, it needs to be processed
+		tokens.pop(); 
 		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(
 				tokens);
 		return new Wrap(paramETN);
