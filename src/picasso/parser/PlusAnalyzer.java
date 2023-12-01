696391fb11ce0dd7a691a9bb9e8455b8dd44af92
JENNA BERNSTEIN <BERNSTEINJ25@mail.wlu.edu>
2023-11-30 19:45:29 -0500

    implement Plus function and create operators package for binary operators class

--- src/picasso/parser/PlusAnalyzer.java
+++ src/picasso/parser/PlusAnalyzer.java
@@ -3,6 +3,7 @@ package picasso.parser;
 import java.util.Stack;
 
 import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.operators.Plus;
 import picasso.parser.tokens.Token;
 
 /**
@@ -20,7 +21,14 @@ public class PlusAnalyzer implements SemanticAnalyzerInterface {
 		// the parameters are the next tokens on the stack.
 		// But, they need to be processed
 		// TODO: Need to finish.
-		return null;
+		System.out.println(tokens);
+		ExpressionTreeNode param1 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		System.out.println(param1);
+		ExpressionTreeNode param2 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		System.out.println(param2);
+		return new Plus(param1, param2);
 	}
 
 }
