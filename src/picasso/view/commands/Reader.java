ac9ac6defe29452c548e272a8c91c7ed7dff411f
lydiazyang <yangl25@mail.wlu.edu>
2023-12-06 10:32:54 -0500

    Preliminary reading from file implementation

--- src/picasso/view/commands/Reader.java
+++ src/picasso/view/commands/Reader.java
@@ -1,9 +1,15 @@
 package picasso.view.commands;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.swing.JFileChooser;
+import javax.swing.JTextField;
 
 import picasso.model.Pixmap;
 import picasso.util.FileCommand;
+import picasso.view.Frame;
 
 /**
  * Open the chosen image file and display in the Pixmap target.
@@ -11,6 +17,7 @@ import picasso.util.FileCommand;
  * @author Robert C Duvall
  */
 public class Reader extends FileCommand<Pixmap> {
+	private Frame frame;
 
 	/**
 	 * Creates a Reader object, which prompts users for image files to open
@@ -19,14 +26,40 @@ public class Reader extends FileCommand<Pixmap> {
 		super(JFileChooser.OPEN_DIALOG);
 	}
 
+	public Reader(Frame frame) {
+		super(JFileChooser.OPEN_DIALOG);
+		this.frame = frame;
+	}
+
 	/**
 	 * Displays the image file on the given target.
 	 */
 	public void execute(Pixmap target) {
 		String fileName = getFileName();
 		if (fileName != null) {
-			target.read(fileName);
+			if (fileName.endsWith(".exp")){
+				readExpressionFromFile(fileName);
+			} else {
+				target.read(fileName);
+			}
 		}
 	}
+
+	private void readExpressionFromFile(String filePath) {
+		
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        	// evaluate each line
+            StringBuilder expressionBuilder = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                expressionBuilder.append(line);
+            }
+
+            String expression = expressionBuilder.toString();
+            frame.setExpressionInTextField(expression);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
