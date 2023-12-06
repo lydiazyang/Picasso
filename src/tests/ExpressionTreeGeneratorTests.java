8c649c1b70bca9ec02cd59bf9c56b7b7dd1d7650
lydiazyang <yangl25@mail.wlu.edu>
2023-12-06 18:35:01 -0500

    Make wrapFunctionTests

--- src/tests/ExpressionTreeGeneratorTests.java
+++ src/tests/ExpressionTreeGeneratorTests.java
@@ -121,4 +121,13 @@ public class ExpressionTreeGeneratorTests {
 		assertEquals(new Clamp(new Addition(new X(), new Y())), e);
 	}
 	
+	@Test
+	public void wrapFunctionTests() {
+		ExpressionTreeNode e = parser.makeExpression("wrap( x )");
+		assertEquals(new Wrap(new X()), e);
+		
+		e = parser.makeExpression("wrap( x + y )");
+		assertEquals(new Wrap(new Addition(new X(), new Y())), e);
+	}
+	
 }
