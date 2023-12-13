793ffe4768dcca0baa82b8aa073b714bdc3fcd60
Lydia Yang <104171393+lydiazyang@users.noreply.github.com>
2023-12-13 02:54:49 -0500

    Revert "Fix syntax errors"
    This reverts commit dc8c21ea4441d8c4393f68dc7f43b1ac7f28c9f1.

--- src/picasso/view/commands/Evaluator.java
+++ src/picasso/view/commands/Evaluator.java
@@ -58,11 +58,13 @@ public class Evaluator implements Command<Pixmap> {
 				}
 			}
 			expressionList.add(input.getText());
-			propertyChangeSupport.firePropertyChange("expressionList", null, expressionList); 
-		} catch (Exception e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "The expression you entered is currently unsupported. Please enter a new expression.", "Parse Exception Error",0, null);}
+			propertyChangeSupport.firePropertyChange("expressionList", null, expressionList);
+		} 
+		}catch (Exception e) {
+      e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "The expression you entered is currently unsupported. Please enter a new expression.", "Parse Exception Error",0, null);
 		}
+	}
 
 	/**
 	 * Convert from image space to domain space.
