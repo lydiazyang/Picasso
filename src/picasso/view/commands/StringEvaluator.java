89a4f01f79bc07e4b147af45558ac5b462af477d
Lydia Yang <104171393+lydiazyang@users.noreply.github.com>
2023-12-13 11:02:28 -0500

    Continue adding implementation for StringEvaluator- create functions to help generate a random function

--- src/picasso/view/commands/StringEvaluator.java
+++ src/picasso/view/commands/StringEvaluator.java
@@ -1,8 +1,16 @@
 package picasso.view.commands;
 
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.Random;
+import java.util.Stack;
 import javax.swing.JTextField;
 import picasso.model.Pixmap;
+import picasso.parser.ExpressionTreeGenerator;
+import picasso.parser.language.ExpressionTreeNode;
 import picasso.util.Command;
+
 
 /**
  * Evaluate a string expression for each pixel in a image.
@@ -24,7 +32,20 @@ public class StringEvaluator implements Command<Pixmap> {
     @Override
     public void execute(Pixmap target) {
         String inputString = input.getText();
-        String generatedExpression = generateBasicExpression(inputString);
+        String generatedExpression = generateExpressionFromString(inputString);
+        ExpressionTreeNode expr = createExpression(generatedExpression);
+
+
+        Dimension size = target.getSize();
+        for (int imageY = 0; imageY < size.height; imageY++) {
+            double evalY = imageToDomainScale(imageY, size.height);
+            for (int imageX = 0; imageX < size.width; imageX++) {
+                double evalX = imageToDomainScale(imageX, size.width);
+                Color pixelColor = expr.evaluate(evalX, evalY).toJavaColor();
+                target.setColor(imageX, imageY, pixelColor);
+            }
+        }
+
     }
 
     /**
@@ -33,15 +54,47 @@ public class StringEvaluator implements Command<Pixmap> {
     * @param input The input string.
     * @return The basic generated expression.
     */
-    private String generateBasicExpression(String input) {
-        StringBuilder expression = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            expression.append(buildOperand(input.charAt(i)));
-            if (i < input.length() - 1) {
-                expression.append(buildOperator(input.charAt(i)));
+    private String generateExpressionFromString(String input) {
+        Stack<String> operators = new Stack<>();
+        Stack<String> operands = new Stack<>();
+        Random random = new Random();
+
+
+        for (char c : input.toCharArray()) {
+            if (isOperatorChar(c)) {
+                operators.push(buildOperator(c));
+            } else {
+                operands.push(buildOperand(c, random));
+            }
+
+
+            if (operands.size() >= 2 && !operators.isEmpty()) {
+                String combinedExpr = combineExpression(operators, operands);
+                operands.push(combinedExpr);
             }
         }
-        return expression.toString();
+
+
+        while (!operators.isEmpty() && operands.size() >= 2) {
+            String combinedExpr = combineExpression(operators, operands);
+            operands.push(combinedExpr);
+        }
+
+
+        while (operands.size() > 1) {
+            String operand2 = operands.pop();
+            String operand1 = operands.pop();
+            String combinedExpr = "(" + operand1 + " + " + operand2 + ")";
+            operands.push(combinedExpr);
+        }
+
+
+        if (operands.isEmpty()) {
+            return "0";
+        } else {
+            return operands.pop();
+        }
+
     }
 
     /**
@@ -51,7 +104,16 @@ public class StringEvaluator implements Command<Pixmap> {
     * @return The operand as a string.
     */
     private String buildOperand(char c) {
-        return "x"; // Placeholder 
+        if (Character.isDigit(c)) {
+            return String.valueOf(c);
+        } else {
+            if (random.nextDouble() < 0.3) { // 30% chance to generate a color
+                return buildRandomColor();
+            } else {
+                return VARIABLES[random.nextInt(VARIABLES.length)];
+            }
+        }
+    }
 
     /**
     * Builds an operator based on a character.
@@ -60,6 +122,30 @@ public class StringEvaluator implements Command<Pixmap> {
     * @return The operator as a string.
     */
     private String buildOperator(char c) {
-        return "+"; // Placeholder =
-}
+        if (lowerCaseChar == 'a') {
+            return "+";
+        } else if (lowerCaseChar == 'e') {
+            return "-";
+        } else if (lowerCaseChar == 'i') {
+            return "*";
+        } else if (lowerCaseChar == 'o') {
+            return "/";
+        } else {
+            return "+"; // Default case
+        }
+    }
+
+    /**
+     * Generates a random RGB color expression.
+     *
+     * @return The color expression as a string.
+     */
+    private String buildRandomColor() {
+        Random random = new Random();
+        double red = random.nextDouble() * 2 - 1;
+        double green = random.nextDouble() * 2 - 1;
+        double blue = random.nextDouble() * 2 - 1;
+        return "[" + red + "," + green + "," + blue + "]";
+    }
+
 }
