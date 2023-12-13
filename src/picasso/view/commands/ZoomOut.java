f5c24bb3be28fac5db5681f4a5e9752a06b5ff52
Han Huynh <huynhh25@mail.wlu.edu>
2023-12-13 00:47:46 -0500

    Implemented zoom out command.

--- src/picasso/view/commands/ZoomOut.java
+++ src/picasso/view/commands/ZoomOut.java
@@ -2,8 +2,6 @@ package picasso.view.commands;
 
 import java.awt.Color;
 import java.awt.Dimension;
-
-import javax.swing.JTextField;
 import javax.swing.JOptionPane;
 
 import picasso.model.Pixmap;
@@ -17,30 +15,63 @@ import picasso.util.Command;
  * @author Han Huynh
  */
 public class ZoomOut implements Command<Pixmap> {
-	public static final double DOMAIN_MIN = -1;
-	public static final double DOMAIN_MAX = 1;
-	private JTextField input;
-	
-	public ZoomOut(JTextField input) {
-		this.input = input;
+	private Evaluator evaluator;
+	public ZoomOut(Evaluator evaluator) {
+		this.evaluator = evaluator;
 	}
 
 	/**
-	 * Evaluate an expression for each point in the image.
+	 * Zooms in 10% of current image
 	 */
-	public void execute(Pixmap target) {
-		
+	public void execute(Pixmap target) {	
+		evaluator.scaleUp();
+		try {
+			// create the expression to evaluate just once
+				// evaluate it for each pixel
+			String input = evaluator.getInput();
+				if (!input.startsWith("//"))  {
+					ExpressionTreeNode expr = createExpression(input);
+					Dimension size = target.getSize();
+					for (int imageY = 0; imageY < size.height; imageY++) {
+						double evalY = imageToDomainScale(imageY, size.height);
+						for (int imageX = 0; imageX < size.width; imageX++) {
+							double evalX = imageToDomainScale(imageX, size.width);
+							Color pixelColor = expr.evaluate(evalX, evalY).toJavaColor();
+							target.setColor(imageX, imageY, pixelColor);
+							}
+					}
+				} 
+			} catch (Exception e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(null, "The expression you entered is currently unsupported. Please enter a new expression.", "Parse Exception Error",0, null);
+			}
 	}
-
+	
 	/**
 	 * Convert from image space to domain space.
 	 */
-
 	protected double imageToDomainScale(int value, int bounds) {
-		double range = DOMAIN_MAX - DOMAIN_MIN;
-		return ((double) value / bounds) * range + DOMAIN_MIN;
+		double scaledMin = evaluator.getScaledMin();
+		double scaledMax = evaluator.getScaledMax();
+		double range = scaledMax - scaledMin;
+		return ((double) value / bounds) * range + scaledMin;
 	}
 
+	/**
+	 * 
+	 * A place holder for a more interesting way to build the expression.
+	 */
+	ExpressionTreeNode createExpression(String function) {
+		// Note, when you're testing, you can use the ExpressionTreeGenerator to
+		// generate expression trees from strings, or you can create expression
+		// objects directly (as in the commented statement below).
+
+		//String test = "x + y";
+
+		ExpressionTreeGenerator expTreeGen = new ExpressionTreeGenerator();
+		return expTreeGen.makeExpression(function);
+		// return new Multiply( new X(), new Y() );
+	}
 
 
 }
