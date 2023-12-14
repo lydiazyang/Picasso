a2e82dabc20ebdd1413e98b55e0e93c686d0a6a2
Lydia Yang <104171393+lydiazyang@users.noreply.github.com>
2023-12-14 01:21:37 -0500

    Create token for PerlinBW function

---
+++ src/picasso/parser/tokens/functions/PerlinBWToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * PerlinBW function token
+ * 
+ */
+public class PerlinBWToken extends FunctionToken {
+
+	public PerlinBWToken() {
+		super("PerlinBW Function Token");
+	}
+
+}
