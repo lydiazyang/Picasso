a44d624bd98ea8c5b5dd9b4cb755a2cf1783fa9f
Lydia Yang <104171393+lydiazyang@users.noreply.github.com>
2023-12-14 01:33:17 -0500

    Complete PerlinBW implementation

---
+++ src/picasso/parser/PerlinBWAnalyzer.java
@@ -0,0 +1,30 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.PerlinBW;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the PerlinBW function.
+ * 
+ * @author Robert C. Duvall
+ * @author Sara Sprenkle
+ * 
+ */
+public class PerlinBWAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Remove the plus token
+		// the parameters are the next tokens on the stack.
+		// But, they need to be processed
+		ExpressionTreeNode param1 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		ExpressionTreeNode param2 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		return new PerlinBW(param2, param1);
+	}
+
+}
