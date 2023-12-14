e44a32d915fb6626e19e0df2dccb6e1c53251ac9
Lydia Yang <104171393+lydiazyang@users.noreply.github.com>
2023-12-14 02:01:42 -0500

    Create new tokens

---
+++ src/picasso/parser/tokens/functions/RgbToYCrCbToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * rgbToYCrCb function token
+ * 
+ */
+public class RgbToYCrCbToken extends FunctionToken{
+
+	public RgbToYCrCbToken() {
+		super("RgbToYCrCb Function Token");
+	}
+
+}
