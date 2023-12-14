1e8102247d4809d8578d40a27dfac8dcb72c526f
Lydia Yang <104171393+lydiazyang@users.noreply.github.com>
2023-12-14 02:14:34 -0500

    Create analyzers

---
+++ src/picasso/parser/RgbToYCrCbAnalyzer.java
@@ -0,0 +1,27 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.RgbToYCrCb;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the RGB to YCrCb function.
+ * 
+ * @author Lydia Yang
+ */
+public class RgbToYCrCbAnalyzer extends UnaryFunctionAnalyzer {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Need to remove the abs token
+		// the parameter is the next token(s) on the stack.
+		// But, it needs to be processed
+		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+
+		return new RgbToYCrCb(paramETN);
+	}
+
+}
